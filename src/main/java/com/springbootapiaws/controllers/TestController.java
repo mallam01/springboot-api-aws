06962ef4c6f4248152c3891998e6cb608d159e6a
Mahmoud R.Allam <m.roushdy.allam@gmail.com>
2024-01-27 23:37:36 -0600

    intial commit

---
+++ src/main/java/com/springbootapiaws/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.springbootapiaws.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String fun(){
+        return "hello!";
+    }
+}
